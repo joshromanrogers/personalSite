a7d8c80392e50a4323735c42be2828eb8dff237f
Josh Rogers <joshromanrogers@gmail.com>
2019-02-04 11:42:30 +0000

    personalised transitions [need to comment + style]

--- js/transition.js
+++ js/transition.js
@@ -11,70 +11,30 @@ var transitionAnimation = Barba.BaseTransition.extend({
     },
 
     startTransition: function () {
-        let oldWrap = this.oldContainer;
-        oldWrap.classList.toggle('fade-out');
-        console.log('fadeOut done');
+
+        var outTransition = new TimelineMax();
+
+        outTransition
+                .set(".transition-blocks-animate", {zIndex:5})
+                .set(".transition-block", {display:'block', x: "-100%"}, "-=0.7")
+                .staggerFromTo(".transition-block", 1.5, {x: "-100%"},{x: "0%", ease: Expo.easeOut}, 0.2, "-=0.7")
+                .staggerFromTo(".transition-block", 1, {x: "0%"},{x: "100%", ease: Expo.easeIn}, 0.2,"-=0.5")
+                .set(".transition-block", {display:'none'})
 
         return new Promise(function (resolve, reject) {
             window.setTimeout(function () {
                 resolve();
-            }, 700);
+            }, 1300);
         });
-        
 
     },
     fadeIn: function () {
-        var newWrap = this.newContainer;
-        newWrap.classList.toggle('fade-in');
+
         this.done();
-        console.log('fadeIn done');
+
     }
 });
 
 Barba.Pjax.getTransition = function () {
     return transitionAnimation;
 };
-
-// startTransition: function () {
-
-//     let outTransition = new TimelineMax();
-
-//     outTransition
-//     .set("transition-block", {display: "block", x: "100%"})
-//     .staggerFromTo("transition-block", 1, {x: "100%"}, {x: "-100%", ease: Expo.easeOut}, 0.2)
-//     .staggerFromTo("transition-block", 1, {x: "-100%"}, {x: "-200%", ease: Expo.easeOut}, 0.2)
-
-//     console.log('blocks transitioned');
-
-//     let oldWrap = this.oldContainer;
-//     oldWrap.classList.toggle('fade-out');
-//     console.log('fadeOut done');
-
-//     return new Promise(function (resolve, reject) {
-//         window.setTimeout(function () {
-//             resolve();
-//         }, 700);
-//     });
-
-// },
-
-// fadeIn: function () {
-//     let oldWrap = this.oldContainer;
-//     let newWrap = this.newContainer;
-//     newWrap.classList.toggle('fade-in');
-
-
-//             TweenMax.set(oldWrap, { display: "none" });
-//             TweenMax.set(newWrap, { visibility: "visible", opacity: 0, });
-           
-           
-//         //     TweenMax.to(".loader", 1, {y:-50, autoAlpha: 0, ease: Expo.easeOut})
-//         //    TweenMax.fromTo(".title", 1.5, {y:30, autoAlpha: 0},{delay:0.8, y:0, autoAlpha: 1, ease: Expo.easeOut})
-
-//             TweenMax.to(newWrap, 0.1, {
-//                 opacity: 1,
-//             });
-
-//             this.done();
-//             console.log('fadeIn done');
-// }
